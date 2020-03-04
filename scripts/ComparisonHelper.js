cb4c6111129aaf517721d0075e9df3ac357f98fa
Rajeev M C <rajeevmc@kite.kerala.gov.in>
2020-03-04 17:09:08 +0530

    Added RegularExpression of Language Lab

---
+++ scripts/ComparisonHelper.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** 
+ * @description Synthesise and Recognise speech from Browser. 
+ */
+class ComparisonHelper {
+    constructor() {
+        this._diffString = "";
+        this._delpatteren = /(?<del><del>\s*(?<delString>(.*?))\s*<\/del>)/gmi;
+        this._inspatteren = /(?<ins><ins>\s*(.*?)\s*<\/ins>)/gmi;
+    }
+
+    /** @description Speak out all the text passed in to the system.
+     * @param {string} message to be spoken.         
+     */
+    diffStrings(sourceString, mutedString) {
+        try {
+            this._diffString = diffString(sourceString, mutedString);
+        } catch (exp) {
+            console.log('exception ocured.', exp.message)
+        }
+    }
+
+    modifyDiffString() {
+        modifiedDiffString = this._diffString.replace(delpatteren, '<a val=$<delString>>$<del></a>');
+        return modifiedDiffString;
+    }
+
+}
