54e4ecb496f908b3b36cc765583cd151340a91e2
Rajeev M C <rajeevmc@kite.kerala.gov.in>
2020-03-05 12:34:36 +0530

    refactored the code

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,45 @@
 'use strict';
-import SpeechHelper from './speechHelper';
 
-export function speak(msg) {
-    let speech = new SpeechHelper();
-    speech.speak(msg);
+let speechHelper = new SpeechHelper();
+let stringDiff = new DiffHelper();
+
+function getTextToRead() {
+    var node = document.getElementById('dvTextToRead');
+    return node.innerText;
 }
+
+function getSpokenText() {
+    var node = document.getElementById('dvSpokenText');
+    return node.innerText;
+}
+
+function delCallBack(data) {
+    console.log(data);
+}
+
+function speakTextCompletionCallBack() {
+    console.log('Read Text Completed');
+}
+
+function speechCallBack(response) {
+    dvSpokenText.innerHTML = response;
+}
+
+btnSpeak.addEventListener('click', () => {
+    let msg = getTextToRead();
+    let speech = new SpeechHelper();
+    speech.speak(msg, speakTextCompletionCallBack);
+});
+
+btnStartRecognise.addEventListener('click', () => {
+    speechHelper.startSpeechRecognition(speechCallBack);
+});
+btnStopRecognise.addEventListener('click', () => {
+    speechHelper.stopSpeechRecognition();
+});
+
+btnAssessReading.addEventListener('click', () => {
+    let sourceText = getTextToRead();
+    let spokenText = getSpokenText();
+    dvDiffArea.innerHTML = stringDiff.getmodifiedDiffString(sourceText, spokenText, 'delCallBack');
+});
